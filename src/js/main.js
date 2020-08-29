037de24a417e589d59961f387f743381ce25120d
kirubel <hagretech@gmail.com>
2020-08-29 15:34:16 +0300

    movies added

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 // Global variabels
-var moviesList = $('.movies .movies__list');
+var moviesList = $('.movies__list'),
+    active = '';
 
+for (i=0; i < moviesList.length;i++){
+    console.log(moviesList[i])
+};
 
 // event listner for the movies animation
-
-moviesList.on('click', function(a){
-    
-})
